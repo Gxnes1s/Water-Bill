e1b2aec26c358ef18bd40cd950e5f8db9f8b7ef2
Gxnes1s <kongmaximum@gmail.com>
2026-09-01 18:24:20 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD2O6pYt84ZpCb8jMkymlOoxZZ52ryQsV8",
+  authDomain: "waterbill-1377f.firebaseapp.com",
+  projectId: "waterbill-1377f",
+  storageBucket: "waterbill-1377f.firebasestorage.app",
+  messagingSenderId: "121569459307",
+  appId: "1:121569459307:web:16cd60f05fe3b503d3052e",
+  measurementId: "G-0K857H6BKB"
+};
